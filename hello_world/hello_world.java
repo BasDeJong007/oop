6582c17b6ce0e0244f739c6f195c6d6b035abe45
The Dude <The Dude@LAPTOP-II3C1STL>
2018-04-18 12:15:56 +0200

    "hello world ding"

---
+++ hello_world/hello_world.java
@@ -0,0 +1,8 @@
+package hello_world;
+
+public class hello_world {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World joo");
+    }
+}
